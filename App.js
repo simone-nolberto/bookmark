981af23e0403f6dfa8b45243fd93d341e9a7be00
simone-nolberto <simone.nolberto@outlook.com>
2024-07-10 17:02:23 +0200

    update with basic input working

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import axios from 'axios';
-// import React, { useState } from 'react';
-// import { Text, TextInput, View } from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Pressable } from 'react-native';
 
-export const state = ({
 
-  userInput: '',
+
+const state = ({
+
   books: [],
   check: true,
   apiBooksUrl: "https://openlibrary.org/search.json?q=",
+  apiAuthorUrl: "https://openlibrary.org/search.json?author=",
 
-  getBooks(url) {
+
+  getBooksbyTitle(url) {
     axios.get(url)
       .then(response => {
+        console.log(url);
         // console.log(response.data.docs);
         this.books = response.data.docs;
         console.log(this.books);
@@ -23,13 +26,38 @@ export const state = ({
 })
 
 
+
+
 export default function App() {
+
+  const [userInput, setState] = React.useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Here we are going to view all of the books</Text>
-      <button onClick={state.getBooks(state.apiBooksUrl + "Fahrenheit+451")}>ricerca</button>
+      <Text>Start typing something here</Text>
+      <TextInput style={styles.input} placeholder='What book are you thinking at?' value={userInput} onChange={(event) => {
+        setState(event.target.value);
+      }}></TextInput>
+      <Button onPress={() => {
+        state.getBooksbyTitle(state.apiBooksUrl + userInput);
+      }} title="Research"></Button>
       <StatusBar style="auto" />
-    </View>
+
+      <View style={styles.row}>
+        {state.books.map((book, index) => (
+
+          <View style={styles.col} >
+            <p key={index}>{book.title}</p>
+
+          </View>
+
+        ))}
+
+
+      </View>
+
+    </View >
+
   );
 
 }
@@ -37,9 +65,38 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 50,
     flex: 1,
+    gap: 3,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  row: {
+    flex: 1,
+    flexWrap: 'wrap'
+
+  },
+
+  col: {
+    flex: 1,
+    gap: 5,
+    justifyContent: 'center',
+    paddingBottom: 5,
+  },
+
+  card: {
+    width: 350,
+    height: 450,
+
+  },
+
+  input: {
+    width: 500,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
