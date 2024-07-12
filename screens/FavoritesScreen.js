e5228397c0f8a18d603b79c491018d03baa27424
simone-nolberto <simone.nolberto@outlook.com>
2024-07-12 14:50:40 +0200

    update with basic fix for "favorites" page

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -22,29 +22,33 @@ export default function FavoriteBooks({ navigation, route }) {
 
         <View style={styles.container}>
 
-            <Text>Here you can find all of the books you saved</Text>
+            {
+
+                route.params.favoriteBook.length > 0 ?
+                    <ScrollView>
+
+                        {route.params.favoriteBooks.map((favoriteBook, index) => (
+                            <View style={styles.card}>
+                                <Text> Title: {favoriteBook.title}</Text>
+                                <Text> Written by: {favoriteBook.author_name}</Text>
+                                <Image style={{ width: 250, height: 250 }} source={state.imageUrl + favoriteBook.cover_i + "-M" + ".jpg"}>
+
+                                </Image>
 
 
-            <ScrollView>
+                                <Button onPress={() => {
+                                    state.removeBookFromFavorites(index);
+                                }} title="Remove" ></Button>
 
-                {route.params.favoriteBooks.map((favoriteBook, index) => (
-                    <View style={styles.card}>
-                        <Text> Title: {favoriteBook.title}</Text>
-                        <Text> Written by: {favoriteBook.author_name}</Text>
-                        <Image style={{ width: 250, height: 250 }} source={state.imageUrl + favoriteBook.cover_i + "-M" + ".jpg"}>
-
-                        </Image>
+                            </View>
+                        ))}
 
 
-                        <Button onPress={() => {
-                            state.removeBookFromFavorites(index);
-                        }} title="Remove" ></Button>
+                    </ScrollView> :
 
-                    </View>
-                ))}
+                    <Text>Here you can find all of the books you saved</Text>
 
-
-            </ScrollView>
+            }
 
         </View >
 
